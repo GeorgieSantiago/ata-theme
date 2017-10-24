960a0ff94b823a76d755a529d524db6e96c65ab1
Georgie Santiago <milestoneartisan@gmail.com>
2017-10-24 20:53:26 +0000

    init commit

---
+++ registration.php
@@ -0,0 +1,11 @@
+<?php
+
+/*
+    Register Ata Theme
+*/
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::THEME,
+    'frontend/RMI/Ata',
+    __DIR__
+);
