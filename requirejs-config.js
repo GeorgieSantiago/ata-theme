82e652e27d166e9b8d69b96cd5895cafad471878
Georgie Santiago <milestoneartisan@gmail.com>
2017-11-02 20:28:34 +0000

    Layout Changes and navigation changes

--- requirejs-config.js
+++ requirejs-config.js
@@ -2,6 +2,7 @@ var config = {
 map: {
          '*': {
              'hotjar': 'js/hotjar',
+             'RMI_ImageCDN/js/swatch-renderer' : 'Magento_Swatches/js/swatch-renderer'
          }
      }
 };
