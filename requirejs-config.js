93b88b13f64c16cfeb4c22ecd70f2f25a5f6bb06
Georgie Santiago <milestoneartisan@gmail.com>
2017-11-02 20:32:32 +0000

    Code cleanup

--- requirejs-config.js
+++ requirejs-config.js
@@ -2,7 +2,6 @@ var config = {
 map: {
          '*': {
              'hotjar': 'js/hotjar',
-             'RMI_ImageCDN/js/swatch-renderer' : 'Magento_Swatches/js/swatch-renderer'
          }
      }
 };
